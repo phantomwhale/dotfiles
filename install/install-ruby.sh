812202da2377e025fb70fed35e85d258f5ea98cd
Ben Turner <ben.turner@pobox.com>
2021-06-21 17:11:00 +1000

    Improve ruby install script
    This ensures I have the tooling installed in all ruby versions

--- install/install-ruby.sh
+++ install/install-ruby.sh
@@ -2,19 +2,23 @@
 
 set -eo pipefail
 
-echo "Installing ruby 2.6"
-export CFLAGS="-Wno-error=implicit-function-declaration" # https://github.com/rbenv/ruby-build/issues/1747
-ruby-install --no-reinstall ruby 2.6
-echo "Installing ruby 2.7"
-ruby-install --no-reinstall ruby 2.7
-echo "Installing latest ruby version"
-ruby-install --no-reinstall ruby
-echo "Changing to latest version"
+
+function install_ruby {
+  local version=$1
+  echo "Installing ruby $version"
+  ruby-install --no-reinstall ruby "$version"
+
+
+  echo "Changing to ruby version $version"
+  chruby ruby $version
+
+  echo "Installing common tools"
+  gem install --no-document timetrap neovim solargraph rufo
+}
+
 source /usr/local/share/chruby/chruby.sh
-chruby ruby
-echo "Installing common tools - timetrap"
-gem install --no-document  timetrap  # https://github.com/samg/timetrap/issues/176
-echo "Installing neovim support gem"
-gem install neovim
-echo "Installing rufo for ruby formatting"
-gem install rufo
+
+export CFLAGS="-Wno-error=implicit-function-declaration" # https://github.com/rbenv/ruby-build/issues/1747
+install_ruby 2.6
+install_ruby 2.7
+install_ruby 3.0

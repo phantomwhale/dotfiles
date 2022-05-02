08be25d989458779b8d652f0a8eefb0030fa3a68
Ben Turner <ben.turner@pobox.com>
2022-05-02 12:10:39 +1000

    Use fork of vim-test to work with NX repositories

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -98,7 +98,7 @@ use {'rizzatti/dash.vim'} -- add :Dash documentation lookup
 use {'rking/ag.vim'} -- add :Ag file search
 use {'junegunn/fzf', dir = '~/.fzf', run = './install --all'}
 use {'ctrlpvim/ctrlp.vim'}
-use {'vim-test/vim-test'}
+use {'phantomwhale/vim-test'}
 use {'majutsushi/tagbar'}
 use {'Raimondi/delimitMate'} -- auto-complete quotes, parens, brackets
 use {'vim-scripts/RelativeNumberCurrentWindow'} -- relative line numbers

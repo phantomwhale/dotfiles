e7c2de9a94512418c016a04621c6a92e8f3b6c65
Ben Turner <ben.turner@pobox.com>
2022-05-02 17:03:46 +1000

    Fix issue with nil parameter in LSP setup
    Looks like the on_attach function was getting called with nil values for
    `bufnr` occasionally, which neovim no longer transforms automatically to
    0 values; instead we do this explicitly now
    
    Ref: https://github.com/neovim/neovim/pull/16745

--- config/nvim/lua/lsp/init.lua
+++ config/nvim/lua/lsp/init.lua
@@ -6,8 +6,8 @@ local tsserver = require("lsp.tsserver")
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr or 0, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr or 0, ...) end
 
   -- Enable completion triggered by <c-x><c-o>
   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')

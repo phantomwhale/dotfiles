37d016248e4eca6a49aab5dda49068903c989e96
Ben Turner <ben.turner@pobox.com>
2022-06-21 20:26:13 +1000

    Simplify install script with -R stow flag

--- install-osx.sh
+++ install-osx.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-STOW_FOLDERS="ag bash bin bundler git javascript kitty neovim ruby timetrap todo zsh"
-
-for dir in $STOW_FOLDERS; do
-	echo "Stowing $dir"
-	stow -D "$dir"
-	stow "$dir"
-done
+stow -R ag bash bin bundler git javascript kitty neovim ruby timetrap todo zsh

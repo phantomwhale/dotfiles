17bafd0331a7fb394bc8b2a5aeb194ee4e600bce
Ben Turner <ben.turner@pobox.com>
2022-11-11 14:31:41 +1100

    Fix server_capabilities LSP properties
    These have been updated to match the camelCase LSP properties. This
    should fix prettier not working (due to tsserver and null-ls both saying
    they were a documentFormattingProvider)
    
    ref: https://neovim.discourse.group/t/how-to-config-multiple-lsp-for-document-hover/3093/2

--- neovim/.config/nvim/lua/lsp/init.lua
+++ neovim/.config/nvim/lua/lsp/init.lua
@@ -45,7 +45,7 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
   buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
 
-  if client.server_capabilities.document_formatting then
+  if client.server_capabilities.documentFormattingProvider then
     vim.api.nvim_exec([[
       augroup LspAutocommands
            autocmd! * <buffer>
@@ -61,7 +61,6 @@ local servers = {'solargraph', 'gopls', 'phpactor'}
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = function(client)
-      client.server_capabilities.document_formatting = true
       on_attach(client)
     end,
     flags = {

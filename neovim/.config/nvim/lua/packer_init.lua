5f3bbb56ccc251666cdb7dce6657d38d2d8ad91b
Ben Turner <ben.turner@pobox.com>
2023-02-27 17:01:09 +1100

    Tighten up packer config
    Mostly using the snippets from the Packer README and removing extra
    cruft I blindly pasted in when setting this up originally

--- neovim/.config/nvim/lua/packer_init.lua
+++ neovim/.config/nvim/lua/packer_init.lua
@@ -1,39 +1,22 @@
-local execute = vim.api.nvim_command
-
--- check if packer is installed (~/.local/share/nvim/site/pack)
 local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-local compile_path = install_path .. "/plugin/packer_compiled.lua"
-local is_installed = vim.fn.empty(vim.fn.glob(install_path)) == 0
 
-if not is_installed then
-  if vim.fn.input("Install packer.nvim? (y for yes) ") == "y" then
-    execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
-    execute("packadd packer.nvim")
-    print("Installed packer.nvim.")
-    is_installed = 1
+local ensure_packer = function()
+  if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+    print "Installing packer..."
+    vim.fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd [[packadd packer.nvim]]
+    return true
   end
+  return false
 end
 
--- Packer commands
-vim.cmd [[command! PackerInstall packadd packer.nvim | lua require('plugins').install()]]
-vim.cmd [[command! PackerUpdate packadd packer.nvim | lua require('plugins').update()]]
-vim.cmd [[command! PackerSync packadd packer.nvim | lua require('plugins').sync()]]
-vim.cmd [[command! PackerClean packadd packer.nvim | lua require('plugins').clean()]]
-vim.cmd [[command! PackerCompile packadd packer.nvim | lua require('plugins').compile()]]
-vim.cmd [[command! PC PackerCompile]]
-vim.cmd [[command! PS PackerStatus]]
-vim.cmd [[command! PU PackerSync]]
+local packer_bootstrap = ensure_packer()
 
-local packer = nil
-if not is_installed then return end
-if packer == nil then
-  packer = require('packer')
-  packer.init({
-      -- we don't want the compilation file in '~/.config/nvim'
-      compile_path = compile_path
-    })
-end
+-- we don't want the compilation file in '~/.config/nvim'
+local compile_path = install_path .. "/plugin/packer_compiled.lua"
 
+local packer = require('packer')
+packer.init({ compile_path = compile_path })
 local use = packer.use
 
 -- Packer can manage itself
@@ -99,10 +82,10 @@ use {'airblade/vim-rooter',
   end
 }
 --use {
-  --"folke/which-key.nvim",
-  --config = function()
-    --require("which-key").setup {}
-  --end
+--"folke/which-key.nvim",
+--config = function()
+--require("which-key").setup {}
+--end
 --}
 
 -- statusline and color scheme
@@ -184,3 +167,8 @@ use {"micarmst/vim-spellsync"} -- keep binary spelling file in sync with text fi
 
 -- Now require the other plugin lua files
 require('plugins/nvim-cmp')
+
+if packer_bootstrap then
+  print "Syncing packer..."
+  packer.sync()
+end

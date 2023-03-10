2ebc4b02f6e569050d782230921caf17e4f6b9a0
Ben Turner <ben.turner@pobox.com>
2023-03-10 12:31:41 +1100

    Use Plenary Job for running kitty command
    This was supposed to fix an issue I was seeing using vim.loop directly,
    but suspect it's still doing the same thing under the hood
    
    In any case, upon selecting a color from the new telescope custom menu,
    sometimes the interface seems to freeze a little, and then every other
    keypress gets swallowed. Only workaround is to `::qq!!` out of vim :(
    
    As it's intermittent, and this isn't a commonly used option, I'll leave
    the code as-is for now, but would be good to understand the problem one
    day

--- neovim/.config/nvim/lua/core/colors.lua
+++ neovim/.config/nvim/lua/core/colors.lua
@@ -1,10 +1,18 @@
 -- this is our source of truth for colorschemes
 local base16_theme_fname = vim.fn.expand(vim.env.HOME ..'/.base16_theme')
 
+-- need to run the file updates async to avoid issues with keypresses not working
+local Job = require "plenary.job"
+
 function notify_colorscheme_changes(name)
     vim.fn.writefile({ name }, base16_theme_fname)
     vim.cmd('colorscheme '..name)
-    vim.loop.spawn('kitty', {
+
+    -- !!! This seems to cause input issues at times
+    --     Error seems to happen when hitting enter on the Telescope menu
+    --     Can live with it for now; this is not a commonly used function
+    job = Job:new({
+        command = 'kitty',
         args = {
             '@',
             'set-colors',
@@ -12,7 +20,8 @@ function notify_colorscheme_changes(name)
             '-a',
             string.format(vim.env.HOME .. '/.config/base16-kitty/colors/%s.conf', name)
         }
-    }, nil)
+    })
+    job:sync()
 end
 
 vim.cmd('colorscheme ' .. vim.fn.readfile(base16_theme_fname)[1])

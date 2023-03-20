dbd62021a432d2a028ba9fdba1e2f4b293f916c5
Ben Turner <ben.turner@pobox.com>
2023-03-20 11:37:09 +1100

    Silence annoying output from stow
    Suppresses the harmless warning lines coming out of stow
    
    Ref: https://github.com/aspiers/stow/issues/65#issuecomment-1465060710

--- install-osx.sh
+++ install-osx.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-stow -R ag bash bin bundler git javascript kitty neovim ruby timetrap todo zsh
+stow -R ag bash bin bundler git javascript kitty neovim ruby timetrap todo zsh 2> >(grep -v 'BUG in find_stowed_path? Absolute/relative mismatch' 1>&2)

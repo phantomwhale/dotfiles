12bc5e07a299932752d77ff3f4f2f55e7beec637
Ben Turner <ben.turner@pobox.com>
2023-05-21 20:18:08 -0700

    Ensure brew bin dir is on the path
    When doing a fresh install of dotfiles, the zshrc file
    won't yet be in place, meaning homebrew bin might not
    yet be on the path for the newer M-class macbooks, which
    install it to /opt/homebrew instead
    
    This guarantees the command is available in all those
    circumstances

--- install-osx.sh
+++ install-osx.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+# need to make sure we have our brew binaries on the PATH; new installs may not have this set up yet!
+
+PATH=$PATH:/opt/homebrew/bin
+
 stow -R ag bash bin bundler git javascript kitty neovim ruby skhd timetrap todo yabai zsh 2> >(grep -v 'BUG in find_stowed_path? Absolute/relative mismatch' 1>&2)

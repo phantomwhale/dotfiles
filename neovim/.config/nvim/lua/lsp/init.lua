673d31bf7040a5b549ec8e125ab28fcbd714a06f
Ben Turner <ben.turner@pobox.com>
2023-11-13 17:12:50 +1100

    Overhauled LSP file
    Mostly adding descriptions, removing custom maps and setting up ruby LSP
    over solargraph. Also set up lua LSP as well

--- neovim/.config/nvim/lua/lsp/init.lua
+++ neovim/.config/nvim/lua/lsp/init.lua
@@ -1,81 +1,152 @@
-local nvim_lsp = require('lspconfig')
-
+local lspconfig = require('lspconfig')
 local cmp_nvim_lsp = require('cmp_nvim_lsp')
-
 local tsserver = require("lsp.tsserver")
 
 vim.cmd("command! LspFormat lua vim.lsp.buf.format( { timeout_ms = 5000 } )")
-vim.keymap.set('n', '<space>f', ':LspFormat<CR>', { noremap=true, silent=true })
+vim.keymap.set('n', '<space>f', ':LspFormat<CR>', { noremap = true, silent = true })
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr or 0, ...) end
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr or 0, ...) end
+
+  local keymap = vim.keymap
 
   -- Enable completion triggered by <c-x><c-o>
   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- commands
-  vim.cmd("command! LspHover          lua vim.lsp.buf.hover()")
-  vim.cmd("command! LspRename         lua vim.lsp.buf.rename()")
-  vim.cmd("command! LspDiagPrev       lua vim.diagnostic.goto_prev()")
-  vim.cmd("command! LspDiagNext       lua vim.diagnostic.goto_next()")
-  vim.cmd("command! LspDiagLine       lua vim.diagnostic.open_float()")
-  vim.cmd("command! LspReferences     lua vim.lsp.buf.references()")
-  vim.cmd("command! LspSignatureHelp  lua vim.lsp.buf.signature_help()")
-  vim.cmd("command! LspDef            lua vim.lsp.buf.definition()")
-  vim.cmd("command! LspTypeDef        lua vim.lsp.buf.type_definition()")
+  -- set keybinds
+  local opts = { noremap = true, silent = true }
 
-  -- Mappings.
-  local opts = { noremap=true, silent=true }
+  opts.desc = "Go to declaration"
+  keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
 
-  buf_set_keymap('n', 'gD',        '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd',        ':LspDef<CR>', opts)
-  buf_set_keymap('n', 'K',         ':LspHover<CR>', opts)
-  buf_set_keymap('n', 'gi',        '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', 'gh',        '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', 'gt',        ':LspTypeDef<CR>', opts)
-  buf_set_keymap('n', '<space>rn', ':LspRename<CR>', opts)
-  buf_set_keymap('n', 'gr',        ':LspReferences<CR>', opts)
-  buf_set_keymap('n', '<space>e',  ':LspDiagLine<CR>', opts)
-  buf_set_keymap('n', '[d',        ':LspDiagPrev<CR>', opts)
-  buf_set_keymap('n', ']d',        ':LspDiagNext<CR>', opts)
+  opts.desc = "Go to definition"
+  keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
 
-  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  opts.desc = "Show documentation for what is under cursor"
+  keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+
+  opts.desc = "Show LSP implementation"
+  keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+
+  opts.desc = "Show method signautre (?)"
+  keymap.set('n', 'gh', vim.lsp.buf.signature_help, opts)
+
+  opts.desc = "Show LSP type definitions"
+  keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
+
+  opts.desc = "Smart rename"
+  keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+
+  opts.desc = "Show LSP references"
+  keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+
+  opts.desc = "Go to previous diagnostic"
+  keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+
+  opts.desc = "Go to next diagnostic"
+  keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+
+  opts.desc = "Show diagnostics in floating window"
+  keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
+
+  opts.desc = "Show documentation for what is under cursor"
+  keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+
+  opts.desc = "Undocumented at this stage"
+  keymap.set('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  keymap.set('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  keymap.set('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
 
   if client.server_capabilities.documentFormattingProvider then
     local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
     vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({ timeout_ms = 5000 })
-        end,
-      })
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format({ timeout_ms = 5000 })
+      end,
+    })
   end
 end
 
 local capabilities = cmp_nvim_lsp.default_capabilities()
 
-nvim_lsp["ruby_ls"].setup({
+_timers = {}
+
+lspconfig.ruby_ls.setup({
   capabilities = capabilities,
-  on_attach = on_attach
+  on_attach = function(client, buffer)
+    on_attach(client, buffer)
+    -- textDocument/diagnostic support until 0.10.0 is released
+    if require("vim.lsp.diagnostic")._enable then
+      return
+    end
+
+    local diagnostic_handler = function()
+      local params = vim.lsp.util.make_text_document_params(buffer)
+      client.request("textDocument/diagnostic", { textDocument = params }, function(err, result)
+        if err then
+          local err_msg = string.format("diagnostics error - %s", vim.inspect(err))
+          vim.lsp.log.error(err_msg)
+        end
+        local diagnostic_items = {}
+        if result then
+          diagnostic_items = result.items
+        end
+        vim.lsp.diagnostic.on_publish_diagnostics(
+          nil,
+          vim.tbl_extend("keep", params, { diagnostics = diagnostic_items }),
+          { client_id = client.id }
+          )
+        end)
+      end
+
+      diagnostic_handler() -- to request diagnostics on buffer when first attaching
+
+      vim.api.nvim_buf_attach(buffer, false, {
+          on_lines = function()
+            if _timers[buffer] then
+              vim.fn.timer_stop(_timers[buffer])
+            end
+            _timers[buffer] = vim.fn.timer_start(200, diagnostic_handler)
+          end,
+          on_detach = function()
+            if _timers[buffer] then
+              vim.fn.timer_stop(_timers[buffer])
+            end
+          end,
+        })
+    end
 })
 
-nvim_lsp["lua_ls"].setup({
-  capabilities = capabilities,
-  on_attach = on_attach
-})
+lspconfig.lua_ls.setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
 
-nvim_lsp["gopls"].setup({
-  capabilities = capabilities,
-  on_attach = on_attach
-})
+    settings = { -- custom settings for lua
+      Lua = {
+        -- make the language server recognize "vim" global
+        diagnostics = {
+          globals = { "vim" },
+        },
+        workspace = {
+          -- make language server aware of runtime files
+          library = {
+            [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+            [vim.fn.stdpath("config") .. "/lua"] = true,
+          },
+        },
+      },
+    }
+  })
+
+lspconfig.gopls.setup({
+    capabilities = capabilities,
+    on_attach = on_attach
+  })
 
 tsserver.setup(on_attach)
 

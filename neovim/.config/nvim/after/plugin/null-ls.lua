86aae4c80275561f0f775969b0f3546a9d978997
Ben Turner <ben.turner@pobox.com>
2024-03-04 17:43:17 +1100

    Remove outdated null_ls plugins
    The none-ls project has pruned some older plugins that didn't get a lot
    of support. One of them; the whitespace trimming, can be done with
    editorconfig which neovim now supports, so this adds that back in
    
    The other two, eslint_d and shellcheck, have alternatives, so I can try
    and install those when I'm next working in those file types.
    
    Ref: https://github.com/nvimtools/none-ls.nvim/issues/58

--- neovim/.config/nvim/after/plugin/null-ls.lua
+++ neovim/.config/nvim/after/plugin/null-ls.lua
@@ -4,15 +4,15 @@ local b = null_ls.builtins
 local sources = {
     b.code_actions.gitsigns,
     b.diagnostics.erb_lint,
-    b.diagnostics.eslint_d,
+    -- b.diagnostics.eslint_d,
     b.diagnostics.write_good,
     b.diagnostics.markdownlint.with({ args = { "--stdin", "--config", os.getenv("HOME") .. "/.markdownlint.jsonc" } }),
-    b.diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
+    -- b.diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
     b.formatting.prettier.with({
         prefer_local = 'node_modules/.bin',
         disabled_filetypes = { "yaml" },
     }),
-    b.formatting.trim_whitespace.with({ filetypes = { "tmux", "teal", "zsh" } }),
+    -- b.formatting.trim_whitespace.with({ filetypes = { "tmux", "teal", "zsh" } }),
     b.formatting.shfmt.with({
         extra_args = { "-i", "2", "-ci" },
     }),

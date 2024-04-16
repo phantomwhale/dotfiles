9369a76dc4702c12478362342dba4d5c695b8613
Ben Turner <ben.turner@pobox.com>
2024-04-16 15:55:57 +1000

    Prevent stow symlinking the bundler folder
    Otherwise the ~/.bundle/cache folder appears in the dotfiles repo, and
    has to be git ignored etc...

--- install-osx.sh
+++ install-osx.sh
@@ -4,4 +4,7 @@
 
 PATH=$PATH:/opt/homebrew/bin
 
-stow -R ag bash bin bundler git javascript kitty neovim ruby skhd timetrap todo yabai zsh 2> >(grep -v 'BUG in find_stowed_path? Absolute/relative mismatch' 1>&2)
+stow --restow ag bash bin git javascript kitty neovim ruby skhd timetrap todo yabai zsh 2> >(grep -v 'BUG in find_stowed_path? Absolute/relative mismatch' 1>&2)
+
+# don't stow the bundle folder, otherwise the bundler cache ends up in the dotfiles repo
+stow --restow --no-folding bundler 2> >(grep -v 'BUG in find_stowed_path? Absolute/relative mismatch' 1>&2)

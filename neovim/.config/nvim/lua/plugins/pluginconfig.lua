5648b1282c73e9859f14ac7c3fddc7565a9ae452
Ben Turner <ben.turner@pobox.com>
2024-10-07 12:42:13 +1100

    Move vim-rails config into lua

--- neovim/.config/nvim/lua/plugins/pluginconfig.lua
+++ neovim/.config/nvim/lua/plugins/pluginconfig.lua
@@ -60,44 +60,4 @@ if !empty(glob("99dev.yml"))
   let g:test#transformation = '99dev'
 endif
 
-let g:rails_projections = {
-    \    "app/controllers/*_controller.rb": {
-    \      "alternate": [
-    \          "spec/api/{}_spec.rb",
-    \          "spec/requests/{}_spec.rb",
-    \          "spec/requests/{}_request_spec.rb",
-    \          "spec/requests/{}_controller_spec.rb",
-    \          "spec/controllers/{}_controller_spec.rb",
-    \      ],
-    \    },
-    \    "spec/requests/*_request_spec.rb": {
-    \      "alternate": [
-    \          "app/controllers/{}_controller.rb",
-    \      ]
-    \    },
-    \    "spec/api/*_spec.rb": {
-    \      "alternate": [
-    \          "app/controllers/{}.rb",
-    \          "app/controllers/{}_controller.rb",
-    \      ]
-    \    },
-    \    "spec/requests/*_spec.rb": {
-    \      "alternate": [
-    \          "app/controllers/{}.rb",
-    \          "app/controllers/{}_controller.rb",
-    \      ]
-    \    },
-    \    "spec/routing/*_routing_spec.rb": {
-    \      "alternate": [
-    \          "app/controllers/{}_controller.rb",
-    \      ]
-    \    },
-    \    "spec/routing/*_spec.rb": {
-    \      "alternate": [
-    \          "app/controllers/{}.rb",
-    \          "app/controllers/{}_controller.rb",
-    \      ]
-    \    },
-    \ }
-
 ]])

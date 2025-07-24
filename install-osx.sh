61b0b9aa784ee5756162231d250e305366a29d51
Ben Turner <ben.turner@pobox.com>
2025-07-24 16:03:55 +1000

    Add ampcode config

--- install-osx.sh
+++ install-osx.sh
@@ -4,7 +4,7 @@
 
 PATH=$PATH:/opt/homebrew/bin
 
-stow --restow ag bash bat bin espanso git javascript kitty neovim ruby skhd timetrap tinty todo yabai zsh 2> >(grep -v 'BUG in find_stowed_path? Absolute/relative mismatch' 1>&2)
+stow --restow ag amp bat bin espanso git javascript kitty neovim ruby skhd timetrap tinty todo yabai zsh 2> >(grep -v 'BUG in find_stowed_path? Absolute/relative mismatch' 1>&2)
 
 # don't stow the bundle folder, otherwise the bundler cache ends up in the dotfiles repo
 stow --restow --no-folding bundler 2> >(grep -v 'BUG in find_stowed_path? Absolute/relative mismatch' 1>&2)

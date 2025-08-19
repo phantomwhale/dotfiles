f8f68e80ccbd90afb581f10a38e690c2e2b261a6
Ben Turner <ben.turner@pobox.com>
2025-08-19 10:04:32 +1000

    Add LSP utils to find ruby tools via multiple methods

--- neovim/.config/nvim/lsp/rubocop.lua
+++ neovim/.config/nvim/lsp/rubocop.lua
@@ -1,5 +1,16 @@
+local lsp_utils = require("modules.lsp-utils")
+local rubocop_exec = lsp_utils.check_executable({
+  { cmd = { "bundle", "exec", "rubocop", "--lsp" },     check = "bundle exec rubocop --version" },
+  { cmd = { "mise", "exec", "--", "rubocop", "--lsp" }, check = "mise exec -- rubocop --version" },
+  { cmd = { "rubocop", "--lsp" },                       check = "rubocop --version" },
+})
+
+table.insert(rubocop_exec, "--ignore-unrecognized-cops")
+table.insert(rubocop_exec, "--config")
+table.insert(rubocop_exec, vim.fn.expand('$HOME/.rubocop.yml'))
+
 return {
-  cmd = { "rubocop", "--lsp", "--ignore-unrecognized-cops", "--config", vim.fn.expand('$HOME/.rubocop.yml') },
+  cmd = rubocop_exec,
   filetypes = { "ruby" },
   root_markers = { "Gemfile", ".git" }
 }
--- neovim/.config/nvim/lsp/ruby_lsp.lua
+++ neovim/.config/nvim/lsp/ruby_lsp.lua
@@ -1,5 +1,11 @@
+local lsp_utils = require("modules.lsp-utils")
+
 return {
-  cmd = { 'ruby-lsp' },
+  cmd = lsp_utils.check_executable({
+    { cmd = { "bundle", "exec", "ruby-lsp" },     check = "bundle exec ruby-lsp --version" },
+    { cmd = { "mise", "exec", "--", "ruby-lsp" }, check = "mise exec -- ruby-lsp --version" },
+    { cmd = { "ruby-lsp" },                       check = "ruby-lsp --version" },
+  }),
   filetypes = { 'ruby', 'eruby' },
   root_markers = { 'Gemfile', '.git' },
   init_options = {

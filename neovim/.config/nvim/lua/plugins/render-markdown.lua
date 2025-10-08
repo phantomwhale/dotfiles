a95f794094ab8bca34c94ecf916f30a7737ad201
Ben Turner <ben.turner@pobox.com>
2025-10-08 22:55:42 +1100

    Use Markdown render for everything non-Obsidian

--- neovim/.config/nvim/lua/plugins/render-markdown.lua
+++ neovim/.config/nvim/lua/plugins/render-markdown.lua
@@ -1,7 +1,14 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
-  filetype = { "codecompanion" }, -- only use for CodeCompanion rendering
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
   opts = {
-    file_types = { 'codecompanion' }
+    ignore = function(buf)
+      -- represents the full path to the buffer
+      local path = vim.api.nvim_buf_get_name(buf)
+      -- returns true if path is inside obsidian vault
+      return string.find(path, "Obsidian") ~= nil
+    end,
   }
 }

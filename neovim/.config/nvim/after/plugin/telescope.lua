9909d09fc17c032075e7e54eef20611ee188d735
Ben Turner <ben.turner@pobox.com>
2025-10-09 00:51:22 +1100

    Move built in keybinds to telescope plugin file

--- neovim/.config/nvim/after/plugin/telescope.lua
+++ neovim/.config/nvim/after/plugin/telescope.lua
@@ -38,8 +38,6 @@ local function telescope_pick_base16_color()
 end
 
 -- Telescope keybinds
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
   -- can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
@@ -47,16 +45,11 @@ vim.keymap.set('n', '<leader>/', function()
     previewer = false,
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sF',
   require('telescope').extensions.dir.find_files, { desc = '[S]earch [F]iles in directory' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sb', telescope_live_grep_open_files, { desc = '[S]earch open [B]uffers' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sG',
   require('telescope').extensions.dir.live_grep, { desc = '[S]earch by [G]rep in directory' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>sr', require('telescope.builtin').git_branches, { desc = '[S]earch git b[R]anches' })
 vim.keymap.set('n', '<leader>sz', require("telescope").extensions.zoxide.list, { desc = '[S]earch [Z]oxide' })
 vim.keymap.set('n', '<leader>sc', telescope_pick_base16_color, { desc = '[S]earch [C]olour themes' })
--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -27,6 +27,15 @@ return {
           cwd_only = true,
         }
       }
+    },
+    keys = {
+      { '<leader>?',       "<cmd>Telescope oldfiles<cr>",     desc = '[?] Find recently opened files' },
+      { '<leader><space>', "<cmd>Telescope buffers<cr>",      desc = '[ ] Find existing buffers' },
+      { '<leader>sf',      "<cmd>Telescope find_files<cr>",   desc = '[S]earch [F]iles' },
+      { '<leader>sh',      "<cmd>Telescope help_tags<cr>",    desc = '[S]earch [H]elp' },
+      { '<leader>sw',      "<cmd>Telescope grep_string<cr>",  desc = '[S]earch current [W]ord' },
+      { '<leader>sd',      "<cmd>Telescope diagnostics<cr>",  desc = '[S]earch [D]iagnostics' },
+      { '<leader>sr',      "<cmd>Telescope git_branches<cr>", desc = '[S]earch git b[R]anches' },
     }
   },
   {

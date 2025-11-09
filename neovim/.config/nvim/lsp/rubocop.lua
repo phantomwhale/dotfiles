84ebc9f223d3fb7cfe0b18ae70a4c9456aeb15dd
Ben Turner <ben.turner@pobox.com>
2025-11-10 09:50:11 +1100

    Add rubocop disable function
    Ref: https://gist.github.com/artemave/078b1dcc7c21c62403394979260954ca

--- neovim/.config/nvim/lsp/rubocop.lua
+++ neovim/.config/nvim/lsp/rubocop.lua
@@ -9,6 +9,53 @@ table.insert(rubocop_exec, "--ignore-unrecognized-cops")
 table.insert(rubocop_exec, "--config")
 table.insert(rubocop_exec, vim.fn.expand('$HOME/.rubocop.yml'))
 
+RD = RD or {}
+
+function RD.join(tbl, sep)
+  local ret = ''
+  for i, v in pairs(tbl) do
+    ret = ret .. i .. sep
+  end
+  return ret:sub(1, - #sep - 1)
+end
+
+function RD.map(tbl, f)
+  local t = {}
+  for k, v in pairs(tbl) do
+    t[f(v)] = true
+  end
+  return t
+end
+
+function RD.rubocop_disable()
+  local current_lnum = vim.api.nvim_win_get_cursor(0)[1]
+  local current_line = vim.api.nvim_get_current_line()
+
+  local diagnostics = vim.diagnostic.get(0, { lnum = current_lnum - 1 })
+  local diagnostic = diagnostics[1]
+
+  if diagnostic and diagnostic.source == 'RuboCop' then
+    local code = RD.join(RD.map(diagnostics, function(d) return d.code end), ', ')
+
+    if diagnostic.lnum == diagnostic.end_lnum then
+      local new_text = current_line .. ' # rubocop:disable ' .. code
+      vim.api.nvim_set_current_line(new_text)
+    else
+      local indent = tonumber(vim.call('indent', current_lnum))
+      local padding = string.rep(' ', indent)
+
+      local enable_text = padding .. '# rubocop:enable ' .. code
+      vim.api.nvim_buf_set_lines(0, diagnostic.end_lnum + 1, diagnostic.end_lnum + 1, false, { enable_text })
+
+      local disable_text = padding .. '# rubocop:disable ' .. code
+      vim.api.nvim_buf_set_lines(0, diagnostic.lnum, diagnostic.lnum, false, { disable_text })
+    end
+  end
+end
+
+vim.api.nvim_set_keymap('n', '<leader>rd', '<cmd>lua RD.rubocop_disable()<CR>', {})
+
+
 return {
   cmd = rubocop_exec,
   filetypes = { "ruby" },

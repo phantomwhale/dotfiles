e87ec104afb5b5949d02f9eb834079df9ec41948
Ben Turner <ben.turner@pobox.com>
2025-11-17 22:34:05 +1100

    Add keymap to open minifiles
    Include a workaround for the Oil magic prefix for it's buffers
    
    Ref: https://github.com/stevearc/oil.nvim/issues/685
    Ref: https://github.com/nvim-lualine/lualine.nvim/blob/3946f0122255bc377d14a59b27b609fb3ab25768/lua/lualine/components/branch/git_branch.lua#L80-L86

--- neovim/.config/nvim/lua/plugins/mini.lua
+++ neovim/.config/nvim/lua/plugins/mini.lua
@@ -1,3 +1,19 @@
+local function open_current_window()
+  local MiniFiles = require('mini.files')
+  local file_dir = vim.api.nvim_buf_get_name(0)
+  -- oil adds an "oil://" prefix to the dir name
+  -- if this is an oil directory, use it's API to get the directory name
+  if package.loaded.oil then
+    local oil = require('oil')
+    local ok, dir = pcall(oil.get_current_dir)
+    if ok and dir and dir ~= '' then
+      file_dir = vim.fn.fnamemodify(dir, ':p:h')
+    end
+  end
+  MiniFiles.open(file_dir, false)
+  MiniFiles.reveal_cwd()
+end
+
 return {
   'echasnovski/mini.nvim',
   version = false,
@@ -7,5 +23,8 @@ return {
     require('mini.files').setup()
     require('mini.operators').setup()
     require('mini.surround').setup()
-  end
+  end,
+  keys = {
+    { "<leader>o", open_current_window, desc = '[O]pen current window' }
+  }
 }

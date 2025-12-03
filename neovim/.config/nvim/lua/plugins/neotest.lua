66cb4d5d0538d650880e3bad23df8bce7ea2de21
Ben Turner <ben.turner@pobox.com>
2025-12-03 13:15:25 +1100

    Remove FixCursorHold
    This was a neovim patch that is now fixed in the core product

--- neovim/.config/nvim/lua/plugins/neotest.lua
+++ neovim/.config/nvim/lua/plugins/neotest.lua
@@ -5,7 +5,6 @@ return {
   dependencies = {
     "nvim-neotest/nvim-nio",
     "nvim-lua/plenary.nvim",
-    "antoinemadec/FixCursorHold.nvim",
     "nvim-treesitter/nvim-treesitter",
     "olimorris/neotest-rspec"
   },

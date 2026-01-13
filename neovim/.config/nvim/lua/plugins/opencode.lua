fe51e65a8eb246331fd7c0f3bc8f59d6aebc2478
Ben Turner <ben.turner@pobox.com>
2026-01-13 14:48:42 +1100

    Fix kitty provider
    --copy-env ensures the ENV gets copied into the subshell, so opencode
    can access things like the correct ruby executables via mise
    
    --port is now mandatory (and added by the plugin too, but we are
    overriding the cmd here, so need to add it in)

--- neovim/.config/nvim/lua/plugins/opencode.lua
+++ neovim/.config/nvim/lua/plugins/opencode.lua
@@ -7,16 +7,17 @@ return {
     { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
   },
   keys = {
-    { "<leader>oa", function() require("opencode").ask("@this: ", { submit = true }) end, mode = { "n", "x" }, desc = "Ask opencode" },
-    { "<leader>ox", function() require("opencode").select() end, mode = { "n", "x" }, desc = "Execute opencode action" },
-    { "<C-.>", function() require("opencode").toggle() end, mode = { "n", "t" }, desc = "Toggle opencode" },
-    { "<S-C-u>", function() require("opencode").command("session.half.page.up") end, desc = "opencode half page up" },
-    { "<S-C-d>", function() require("opencode").command("session.half.page.down") end, desc = "opencode half page down" },
+    { "<leader>oa", function() require("opencode").ask("@this: ", { submit = true }) end, mode = { "n", "x" },             desc = "Ask opencode" },
+    { "<leader>ox", function() require("opencode").select() end,                          mode = { "n", "x" },             desc = "Execute opencode action" },
+    { "<C-.>",      function() require("opencode").toggle() end,                          mode = { "n", "t" },             desc = "Toggle opencode" },
+    { "<S-C-u>",    function() require("opencode").command("session.half.page.up") end,   desc = "opencode half page up" },
+    { "<S-C-d>",    function() require("opencode").command("session.half.page.down") end, desc = "opencode half page down" },
   },
   config = function()
     ---@type opencode.Opts
     vim.g.opencode_opts = {
       provider = {
+        cmd = "--copy-env opencode --port",
         enabled = "kitty",
         kitty = {
         }

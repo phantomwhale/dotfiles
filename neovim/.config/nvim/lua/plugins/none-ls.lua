68cb81fb643533186d45608f99883b78a5524cb4
Ben Turner <ben.turner@pobox.com>
2026-06-03 17:25:14 +1000

    Consolidate none-ls config into plugins/none-ls.lua
    Previously the plugin spec was empty and the actual sources/setup lived
    in after/plugin/none-ls.lua, which runs eagerly on startup and defeats
    lazy.nvim. The setup now lives in the plugin spec's config function,
    guarded by event = BufReadPre/BufNewFile so it loads on first file open.
    The standalone after/plugin file is removed.

--- neovim/.config/nvim/lua/plugins/none-ls.lua
+++ neovim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,8 +1,39 @@
 return {
   -- use neovim as a custom language server for diagnostics, code actions and more
   'nvimtools/none-ls.nvim',
+  event = { "BufReadPre", "BufNewFile" },
   dependencies = {
     'nvim-lua/plenary.nvim',
     'lewis6991/gitsigns.nvim'
-  }
+  },
+  config = function()
+    local none_ls = require("null-ls")
+    local b = none_ls.builtins
+
+    none_ls.setup({
+      sources = {
+        b.code_actions.gitsigns,
+        b.diagnostics.write_good,
+        b.diagnostics.markdownlint,
+        -- b.diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
+        b.formatting.prettier.with({
+          prefer_local = 'node_modules/.bin',
+          disabled_filetypes = { "yaml", "markdown" },
+        }),
+
+        -- b.formatting.trim_whitespace.with({ filetypes = { "tmux", "teal", "zsh" } }),
+        b.formatting.shfmt.with({
+          extra_args = { "-i", "2", "-ci" },
+        }),
+        b.formatting.phpcsfixer.with({
+          env = {
+            PHP_CS_FIXER_IGNORE_ENV = "1"
+          },
+        }),
+      },
+      flags = {
+        debounce_text_changes = 5000,
+      }
+    })
+  end,
 }

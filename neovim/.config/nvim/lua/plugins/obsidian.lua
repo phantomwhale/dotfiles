fc8a1fd09b7979cdb7a2d45e9db25ed82f2e9243
Ben Turner <ben.turner@pobox.com>
2026-06-08 02:58:19 +1000

    Migrate obsidian.nvim to the maintained community fork

--- neovim/.config/nvim/lua/plugins/obsidian.lua
+++ neovim/.config/nvim/lua/plugins/obsidian.lua
@@ -1,22 +1,23 @@
 return {
-  "epwalsh/obsidian.nvim",
+  "obsidian-nvim/obsidian.nvim",
   version = "*", -- recommended, use latest release instead of latest commit
   event = {
     "BufReadPre " .. vim.fn.expand "~" .. "/GoogleDrive/Obsidian/*.md",
     "BufNewFile " .. vim.fn.expand "~" .. "/GoogleDrive/Obsidian/*.md",
   },
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
   keys = {
-    { "<leader>so", ":ObsidianSearch<CR>", desc = "Search obsidian files" },
+    { "<leader>so", ":Obsidian search<CR>", desc = "Search obsidian files" },
   },
   opts = {
+    legacy_commands = false,
     workspaces = {
       {
         name = "personal",
         path = "~/GoogleDrive/Obsidian",
       },
+    },
+    picker = {
+      name = "snacks.picker",
     },
   },
 }
--- neovim/.config/nvim/lua/plugins/render-markdown.lua
+++ neovim/.config/nvim/lua/plugins/render-markdown.lua
@@ -4,12 +4,5 @@ return {
   dependencies = { 'nvim-mini/mini.nvim' },
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
-  opts = {
-    ignore = function(buf)
-      -- represents the full path to the buffer
-      local path = vim.api.nvim_buf_get_name(buf)
-      -- returns true if path is inside obsidian vault
-      return string.find(path, "Obsidian") ~= nil
-    end,
-  }
+  opts = {}
 }
